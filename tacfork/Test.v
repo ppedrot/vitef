d485bb53f6f07df0fccd7d6cc01218b547e7c652
Pierre-Marie Pédrot <pierre-marie.pedrot@inria.fr>
2013-06-20 20:01:29 +0200

    Allow different tactics in each branch

--- tacfork/Test.v
+++ tacfork/Test.v
@@ -1,11 +1,16 @@
 Require Fork.
 
+Goal True /\ True.
+Proof.
+Time fork split >> [trivial|trivial].
+Qed.
+
 Goal
   (forall A : Prop, A -> A) /\
   False /\
   (forall A : Prop, False -> A) /\
   (forall A : Prop, A -> A -> A).
 Proof.
-fork (repeat apply conj) >> (intuition eauto).
+Time fork (repeat apply conj) >> (intuition eauto).
 admit.
 Qed.
--- tacfork/g_tacfork.ml4
+++ tacfork/g_tacfork.ml4
@@ -1,5 +1,6 @@
 open Fork
 
 TACTIC EXTEND fork
-  | ["fork" tactic(init) ">>" tactic(tac) ] -> [ fork_tac init tac ]
+  | ["fork" tactic(init) ">>" tactic(tac) ] -> [ fork_tac init (One tac) ]
+  | ["fork" tactic(init) ">>" "[" tactic_list_sep(tacs, "|") "]" ] -> [ fork_tac init (Many tacs) ]
 END;;
